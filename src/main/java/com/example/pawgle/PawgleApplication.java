8f02acfb24c1e2a4032034ebe926707652328ddd
SeungBum Baek <wer06099@naver.com>
2024-07-21 17:09:44 +0900

    Add(BSB) : í초기 세팅

---
+++ src/main/java/com/example/pawgle/PawgleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawgle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawgleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawgleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pawgle/PawgleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawgle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawgleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
